0d7aac23a54e1079ff9bbaceb9dc2c597d6a3a88
wangshaocong <wangshaocong@megvii.com>
2024-07-31 18:32:13 +0800

    tmp:add thread test

--- src/test/thread/main.cpp
+++ src/test/thread/main.cpp
@@ -1,10 +1,27 @@
 #include "thread/mul_thread_error.h"
-
-
-
+#include <iostream>
 
 int main()
 {
     thread::multi_thread_error m;
-    m.run(100);
+    int                        count = 10;
+    auto                       now1  = std::chrono::system_clock::now();
+    for (auto i = 0; i < count; i++) {
+        m.run(100000000, thread::multi_thread_error::run_type<false, true>());
+    }
+    auto now2 = std::chrono::system_clock::now();
+    std::cout << "no one core mut spend time :" << (now2 - now1).count();
+
+    for (auto i = 0; i < count; i++) {
+        m.run(100000000, thread::multi_thread_error::run_type<false, false>());
+    }
+    auto now3 = std::chrono::system_clock::now();
+    std::cout << "no one core no mut spend time :" << (now3 - now2).count();
+
+    for (auto i = 0; i < count; i++) {
+        m.run(100000000, thread::multi_thread_error::run_type<true, false>());
+    }
+    auto now4 = std::chrono::system_clock::now();
+    std::cout << "one core no mut spend time :" << (now4 - now3).count();
+    return 0;
 }
