1904002ecda185d4893486f2225969b36c33f865
wangshaocong <wangshaocong@megvii.com>
2025-01-14 16:30:39 +0800

    feat:test comit tiny

--- src/test/eigen/sum_matrix/sum_matrix.cpp
+++ src/test/eigen/sum_matrix/sum_matrix.cpp
@@ -3,13 +3,10 @@
 #include <iostream>
 #include <vector>
 
-
- Eigen::Matrix3d SumMartix::Sum(const Eigen::Matrix3d &a,const Eigen::Matrix3d &b)
- {
-    std::vector<int> vec{0,1,2,3,4};
-    for(auto i =0;i<vec.size();i++)
-    {
-        std::cout<<vec[i]<<std::endl;
+Eigen::Matrix3d SumMartix::Sum(const Eigen::Matrix3d &a, const Eigen::Matrix3d &b) {
+    std::vector<int> vec{0, 1, 2, 3, 4};
+    for (auto i = 0; i < vec.size() + 1; i++) {
+        std::cout << vec[ i ] << std::endl;
     }
-     return a + b;
- }
+    return a + b;
+}
